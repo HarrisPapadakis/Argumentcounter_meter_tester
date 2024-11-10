671a0b2695c823d7d6a3c43ba26cc429403e5816
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2024-11-10 13:13:18 +0200

    Add files via upload

---
+++ hope_u_having_great_day.py
@@ -0,0 +1,8 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========== RESTART: C:/Users/harri/Desktop/hope_u_having_great_day.py ==========
+Hello, Alice! Hope you are having a great day!
+Hello, Bob! Hope you are having a great day!
+Hello, Charlie! Hope you are having a great day!
+Hello, Diana! Hope you are having a great day!

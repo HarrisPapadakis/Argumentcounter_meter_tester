b977165576a250ca90416e7ceb1b41f3ea01a509
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2024-12-17 22:48:01 +0200

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,11 @@
+#In this program we have tuples
+dimensions = (200, 50)
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+#Here we create new tuple with new elements
+dimensions = (400, 100)  
+print("\nModified new dimensions:")
+for dimension in dimensions:
+    print(dimension)

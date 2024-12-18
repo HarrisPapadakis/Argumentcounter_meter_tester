adf9ab6e8ee196c6c9487f76d411459ec0534788
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2024-12-18 17:44:14 +0200

    Add files via upload

---
+++ banned_users.py
@@ -0,0 +1,5 @@
+banned_users = ["toilet_duck", "archiblame", "nightstalker", "blamer"] #create the list with given values
+user = "melanie" # name of user that is not in the list
+
+if user not in banned_users: #check the condition with boolean
+    print(f"{user.title()} , you are authorized and you have permition to write if you wish")

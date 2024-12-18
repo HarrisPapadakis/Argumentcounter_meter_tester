1c0249c1e9465c242d3020b6f4362b90c5bf0e5c
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2024-12-18 17:29:17 +0200

    Add files via upload

---
+++ requested_topping_pizza.py
@@ -0,0 +1,4 @@
+requested_toppings = ["mushroom", "onions", "pineapple"]
+
+print("mushroom" in requested_toppings) #check if there's mushroom  in the requested_topping(list)
+print("pepperoni" in requested_toppings)#check if there' s pepperoni  in the requested_topping(list)

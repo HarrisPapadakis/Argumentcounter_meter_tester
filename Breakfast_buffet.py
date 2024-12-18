128a718fe35a342c50db6fd0b3422c4add8e4e17
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2024-12-18 14:27:40 +0200

    Add files via upload

---
+++ Breakfast_buffet.py
@@ -0,0 +1,4 @@
+morning_buffet = ("English Breakfast", "Continental Breakfast", "American Breakfast", "Greek Breakfast", "Ala cart" )
+print("The options for the buffet for the first two weeks of each month are:")
+for breakfast in  morning_buffet:
+    print(breakfast)

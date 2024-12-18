31a559649243007541cb8ca34d6a8d7102b7e821
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2024-12-18 17:09:36 +0200

    Add files via upload

---
+++ cars.py
@@ -0,0 +1,8 @@
+cars = ["suzuki", "seat", "ford", "toyota", "bmw", "mercedes"]
+
+for car in cars :
+    if car in cars == "bmw":
+        print(car.upper())
+    else:
+        print(car.title())
+    

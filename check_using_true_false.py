bd91b19798b862b18de767e38812d454e1e20497
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2024-12-18 19:54:28 +0200

    Add files via upload

---
+++ check_using_true_false.py
@@ -0,0 +1,35 @@
+car = 'subaru'
+print("Is car == 'subaru'?I predict True.")
+print (car == 'subaru') #true
+
+print("\nIs car == 'audi'?I predict False.")
+print(car == 'audi') #False
+
+#More checks
+age = 25
+print("\nIs your age > 20?I predict True.")
+print (age > 20) #true
+
+print("\nIs your age < 18?I predict False .")
+print (age < 18 ) #False
+
+colour = 'blue'
+print("\nIs your colour == 'blue'?I predict True.")
+print(colour == 'blue')#True
+
+print("\nIs colour == 'red'?I predict False.")
+print(colour == 'red')#False
+
+height = 180
+print("\nIs height >= 170?I predict True.")
+print(height >= 170)#True
+
+print("\nIs height < '150'?I predict False.")
+print(height < 150)#False
+
+name = 'Alice'
+print("\nIs name == 'Alice'?I predict True.")
+print(name == 'Alice')#True
+
+print("\nIs name == 'Bob'?I predict False.")
+print(name == 'Bob')#False

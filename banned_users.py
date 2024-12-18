e0429a8aea287fc61251551fcd799b43c02bbc5a
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2024-12-18 17:48:43 +0200

    Update banned_users.py

--- banned_users.py
+++ banned_users.py
@@ -2,4 +2,4 @@ banned_users = ["toilet_duck", "archiblame", "nightstalker", "blamer"] #create t
 user = "melanie" # name of user that is not in the list
 
 if user not in banned_users: #check the condition with boolean
-    print(f"{user.title()} , you are authorized and you have permition to write if you wish")
+    print(f"{user.title()} , you are authorized and you have permition to write if you wish") #print response

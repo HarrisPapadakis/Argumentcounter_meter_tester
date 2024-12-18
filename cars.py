7d3b9a0b2f4dc607d9a9a170d7c7571fbde93919
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2024-12-18 17:11:54 +0200

    Update cars.py

--- cars.py
+++ cars.py
@@ -1,8 +1,7 @@
 cars = ["suzuki", "seat", "ford", "toyota", "bmw", "mercedes"]
 
-for car in cars :
-    if car in cars == "bmw":
-        print(car.upper())
+for car in cars:
+    if car == "bmw":
+        print(car.upper())  # Print in uppercase if the car is "bmw"
     else:
-        print(car.title())
-    
+        print(car.title())  # Print in title case for other cars

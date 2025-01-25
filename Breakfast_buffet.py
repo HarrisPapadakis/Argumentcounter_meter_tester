ff39c63520abb392c3da587bb887865436766793
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2025-01-25 21:12:57 +0200

    Update Breakfast_buffet.py

--- Breakfast_buffet.py
+++ Breakfast_buffet.py
@@ -1,4 +1,9 @@
+# Define a tuple containing different breakfast options for the buffet
 morning_buffet = ("English Breakfast", "Continental Breakfast", "American Breakfast", "Greek Breakfast", "Ala cart" )
+
+# Print a message informing about the available buffet options
 print("The options for the buffet for the first two weeks of each month are:")
+
+# Loop through the tuple and print each breakfast option
 for breakfast in  morning_buffet:
     print(breakfast)
